6553182d04bf9bf90d620d4dea6aa95316e5c313
Akhilesh Soni <49095481+akhilswin@users.noreply.github.com>
2020-09-22 16:58:57 +0530

    Update MainClass.java

--- src/main/java/yaksha/MainClass.java
+++ src/main/java/yaksha/MainClass.java
@@ -1,12 +1,26 @@
 package yaksha;
 
+import java.util.Scanner;
+
 public class MainClass {
 	public static void main(String[] args) {
 		int i;
-		int[] arr = { 90, 23, 101, 45, 65, 23, 67, 89, 34, 23 };
-		int result[]=QuickSort.quickSort(arr, 0, 9);
+		Scanner scanner = new Scanner(System.in);
+		int[] array1;
+		int sizeOfArray;
+		System.out.println("Enter size of the Array :");
+		sizeOfArray = scanner.nextInt();
+		array1 = new int[sizeOfArray];
+		System.out.println("Enter numbers :");
+		for (i = 0; i < array1.length; i++) {
+			array1[i] = scanner.nextInt();
+		}
+		Array array = new Array(array1, 0, array1.length - 1);
+		int result[] = QuickSort.quickSort(array);
 		System.out.println("\n The sorted array is: \n");
-		for (i = 0; i < 10; i++)
-			System.out.println(result[i]);///
+		for (i = 0; i < 10; i++) {
+			System.out.println(result[i]);
+		}
+		scanner.close();
 	}
 }
