013157665dafc678340f474a3bf6967a35f5a03e
Akhilesh Soni <49095481+akhilswin@users.noreply.github.com>
2020-09-22 17:00:24 +0530

    Update TestMainClass.java

--- src/test/java/yaksha/TestMainClass.java
+++ src/test/java/yaksha/TestMainClass.java
@@ -1,14 +1,11 @@
 package yaksha;
 
-import static org.junit.Assert.assertArrayEquals;
 import static yaksha.TestUtils.businessTestFile;
 import static yaksha.TestUtils.currentTest;
 import static yaksha.TestUtils.yakshaAssert;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class TestMainClass {
@@ -29,7 +26,9 @@ public class TestMainClass {
 		Integer expectedResult[] = { 23, 23, 23, 34, 45, 65, 67, 89, 90, 101 };
 		List<Integer> expectedResultList = Arrays.asList(expectedResult);
 		int[] arr = { 90, 23, 101, 45, 65, 23, 67, 89, 34, 23 };
-		int result[] = QuickSort.quickSort(arr, 0, 9);
+		// Input : 90 23 101 45 65 23 67 89 34 23
+		Array array = new Array(arr, 0, arr.length - 1);
+		int result[] = QuickSort.quickSort(array);
 		List<Integer> resultList = new ArrayList<Integer>(result.length);
 		for (int i : result) {
 			resultList.add(i);
